df68f26dd01aa7a876de21782d68a4950a8a72c0
Dia6lo <Dgamjm@gmail.com>
2016-03-16 14:43:58 +1000

    Implement static Pixi members.

---
+++ Bridge.Pixi/DrawModes.cs
@@ -0,0 +1,33 @@
+﻿namespace Bridge.Pixi
+{
+	/// <summary>
+	/// Various webgl draw modes. These can be used to specify which GL DrawMode
+	/// to use under certain situations and renderers.
+	/// </summary>
+	[External]
+	[Name("PIXI.DRAW_MODES")]
+	[Enum(Emit.Name)]
+	public enum DrawModes
+	{
+		[Name("POINTS")]
+		Points,
+
+		[Name("LINES")]
+		Lines,
+
+		[Name("LINE_LOOP")]
+		LineLopp,
+
+		[Name("LINE_STRIP")]
+		LineStrip,
+
+		[Name("TRIANGLES")]
+		Triangles,
+
+		[Name("TRIANGLE_STRIP")]
+		TriangleStrip,
+
+		[Name("TRIANGLE_FAN")]
+		TriangleFan,
+	}
+}
--- Bridge.Pixi/Pixi.cs
+++ Bridge.Pixi/Pixi.cs
@@ -1,4 +1,6 @@
-﻿namespace Bridge.Pixi
+﻿using Bridge.Pixi.Loaders;
+
+namespace Bridge.Pixi
 {
 	[External]
 	[Namespace(false)]
@@ -27,6 +29,54 @@
 		[FieldProperty]
 		[Name("TARGET_FPMS")]
 		public static float TargetFPMS { get; set; }
+
+		/// <summary>
+		/// The default render options if none are supplied to WebGLRenderer or CanvasRenderer.
+		/// </summary>
+		[FieldProperty]
+		[Name("DEFAULT_RENDER_OPTIONS")]
+		public static RendererOptions DefaultRendererOptions { get; }
+
+		/// <summary>
+		/// Constant conversion factor for converting degrees to radians.
+		/// </summary>
+		[FieldProperty]
+		[Name("DEG_TO_RAD")]
+		public static float DegToRad { get; }
+
+		/// <summary>
+		/// A premade instance of the loader that can be used to loader resources.
+		/// </summary>
+		[FieldProperty]
+		public static Loader Loader { get; set; }
+
+		/// <summary>
+		/// Two Pi.
+		/// </summary>
+		[FieldProperty]
+		[Name("PI_2")]
+		public static float Pi2 { get; }
+
+		/// <summary>
+		/// Constant conversion factor for converting radians to degrees.
+		/// </summary>
+		[FieldProperty]
+		[Name("RAD_TO_DEG")]
+		public static float RadToDeg { get; }
+
+		/// <summary>
+		/// The prefix that denotes a URL is for a retina asset.
+		/// </summary>
+		[FieldProperty]
+		[Name("RETINA_PREFIX")]
+		public static string RetinaPrefix { get; }
+
+		/// <summary>
+		/// String of the current PIXI version.
+		/// </summary>
+		[FieldProperty]
+		[Name("VERSION")]
+		public static string Version { get; }
 
 		/// <summary>
 		/// This helper function will automatically detect which renderer you should be using.
